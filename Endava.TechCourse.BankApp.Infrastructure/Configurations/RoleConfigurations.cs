336874da6045675f729ab86dc9af8379d3abb912
lenapapuc <elenapapuc99@gmail.com>
2023-11-08 17:41:13 +0200

    Added user roles seeding

--- Endava.TechCourse.BankApp.Infrastructure/Configurations/RoleConfigurations.cs
+++ Endava.TechCourse.BankApp.Infrastructure/Configurations/RoleConfigurations.cs
@@ -10,6 +10,10 @@ using System.Threading.Tasks;
 
 namespace Endava.TechCourse.BankApp.Infrastructure.Configurations
 {
+    /// <summary>
+    /// we can do this every time we want to initialize the database with some data, for example an admin user, don't forget 
+    /// to add into the Application DbContext
+    /// </summary>
     public class RoleConfigurations : IEntityTypeConfiguration<IdentityRole<Guid>>
     {
         public void Configure(EntityTypeBuilder<IdentityRole<Guid>> builder)
